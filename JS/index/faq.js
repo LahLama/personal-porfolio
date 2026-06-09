4dff56e5597ce5ad5e49edd2683e11614c7a392f
LahLama <jamesihlenfeldt1@gmail.com>
2026-06-09 14:25:36 +0200

    faq.js more intuitive control

--- JS/index/faq.js
+++ JS/index/faq.js
@@ -1,12 +1,12 @@
 
 
-const questions = document.querySelectorAll(".faq_question") 
+const questions = document.querySelectorAll(".faq_q") 
 questions.forEach(element => {
     element.addEventListener("click", showAnswer)
 });
 
 function showAnswer(faq_Q) {
-    this.classList.toggle("rotated");
+    this.querySelector(".faq_question") .classList.toggle("rotated");
     const answer = this.parentElement.querySelector(".faq_answer");
     answer.classList.toggle("hidden")
     console.log(answer);
